e1731c6f45071544a1e17a73c9cac11b27f9067d
Charlotte Smail <smail.charlotte@gmail.com>
2019-02-15 15:33:23 -0500

    Create difference MySql user

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
     require_once('Database.php');
     $database_name='lead_housing';
-    $username='root';
+    $username='headsupcity';
     $password='Temp@12345';
     $host='localhost';
     $db = new Database($database_name, $username, $password, $host);
@@ -10,4 +10,3 @@
     define('MAX_INSTANCE',3);
     date_default_timezone_set("America/Los_Angeles");
 ?>
-
