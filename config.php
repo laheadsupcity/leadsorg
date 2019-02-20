095e9d80c92720a3100ed8cfa781f0ef527534a6
Charlotte Smail <smail.charlotte@gmail.com>
2019-02-20 09:34:18 -0500

    Change MySql user to headsupcity

--- config.php
+++ config.php
@@ -1,7 +1,8 @@
 <?php
     require_once('Database.php');
+    require_once('debug.php');
     $database_name='lead_housing';
-    $username='root';
+    $username='headsupcity';
     $password='Temp@12345';
     $host='localhost';
     $db = new Database($database_name, $username, $password, $host);
