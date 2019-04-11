f57899c5db889da0e7db0e28aed7ff5fc54c56d6
Charlotte Smail <smail.charlotte@gmail.com>
2019-04-11 09:38:17 -0400

    Exclude adding properties to a favorite folder which are already in the folder

--- add_to_favorites_folder.php
+++ add_to_favorites_folder.php
@@ -4,6 +4,23 @@ require_once('config.php');
 $folder_id = $_POST['folder_id'];
 $parcel_numbers = explode(',', $_POST['parcel_numbers']);
 
+$db->select(
+  'favorite_properties',
+  array(
+    'parcel_number' => $parcel_numbers,
+    'folder_id' => $folder_id
+  )
+);
+
+$apns_to_exclude = array_map(
+  function ($property) {
+    return $property->parcel_number;
+  },
+  $db->result()
+);
+
+$parcel_numbers = array_diff($parcel_numbers, $apns_to_exclude);
+
 foreach ($parcel_numbers as $parcel_number) {
   $db->insert(
     'favorite_properties',
